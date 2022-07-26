4511e526d70f9c143afee854654ecf409df9c8bd
genki28 <taiiku39@gmail.com>
2022-07-27 00:42:52 +0900

    諸々設定周りの修正

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -45,7 +45,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: process.env.API_BASE_URL,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
