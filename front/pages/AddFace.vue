b53f3a960caa5a4a5e7f8410fa686e3452d95706
genki28 <taiiku39@gmail.com>
2022-08-01 02:06:45 +0900

    不要になったためコメントアウト

--- front/pages/AddFace.vue
+++ front/pages/AddFace.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <h1>顔写真をupload</h1>
-    <label>
+    <!-- MEMO: ※こちらのサンプルページはすでに必要無くなったため、コメントアウト。ただし、再度必要になったときのため、一旦コメントアウト  -->
+    <!-- <label>
       顔写真
       <input type="file" @change="selectFile" />
     </label>
@@ -9,40 +10,37 @@
       CollectionName
       <input v-model="collectionName" type="text" />
     </label>
-    <button @click="sendImage">顔写真を登録する</button>
+    <button @click="sendImage">顔写真を登録する</button> -->
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@nuxtjs/composition-api'
-import axios from 'axios'
+import { defineComponent } from '@nuxtjs/composition-api'
+// import axios from 'axios'
 
 export default defineComponent({
   name: 'AddFace',
   setup() {
-    const file = ref<File>()
-    const collectionName = ref<string>('')
-
-    const sendImage = () => {
-      const data = new FormData()
-      data.append('file', file.value)
-      data.append('collection_name', collectionName.value)
-      axios.post('http://localhost:8080/add-face-to-collection', data, {
-        headers: {
-          'content-type': 'multipart/form-data',
-        },
-      })
-    }
-
-    const selectFile = (event: { target: HTMLInputElement }) => {
-      file.value = event.target.files[0]
-    }
-
-    return {
-      collectionName,
-      sendImage,
-      selectFile,
-    }
+    // const file = ref<File>()
+    // const collectionName = ref<string>('')
+    // const sendImage = () => {
+    //   const data = new FormData()
+    //   data.append('file', file.value)
+    //   data.append('collection_name', collectionName.value)
+    //   axios.post('http://localhost:8080/add-face-to-collection', data, {
+    //     headers: {
+    //       'content-type': 'multipart/form-data',
+    //     },
+    //   })
+    // }
+    // const selectFile = (event: { target: HTMLInputElement }) => {
+    //   file.value = event.target.files[0]
+    // }
+    // return {
+    //   collectionName,
+    //   sendImage,
+    //   selectFile,
+    // }
   },
 })
 </script>
